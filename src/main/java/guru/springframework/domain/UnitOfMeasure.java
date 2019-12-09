cbe68718436403c88cbb9419ff0513abd0f0adff
Mark Thew <morales.markthew@gmail.com>
2019-12-09 22:28:54 +0800

    added database initializer

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -11,7 +11,7 @@ public class UnitOfMeasure {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;	
-	private String uom;
+	private String description;
 
 	public Long getId() {
 		return id;
@@ -19,11 +19,11 @@ public class UnitOfMeasure {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getUom() {
-		return uom;
+	public String getDescription() {
+		return description;
 	}
-	public void setUom(String uom) {
-		this.uom = uom;
+	public void setDescription(String description) {
+		this.description = description;
 	}
-		
+	
 }

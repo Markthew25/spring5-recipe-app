02bad7fb02232aa933a2f252c79f12996f09a62c
Mark Thew <morales.markthew@gmail.com>
2019-12-17 19:52:51 +0800

    added lombok annotation

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -5,10 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by jt on 6/1/17.
  */
+@Slf4j
 @Controller
 public class IndexController {
 	
@@ -20,7 +22,7 @@ public class IndexController {
 
 	@RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
-    	
+    	log.debug("I'm in index requestMapping");
 		model.addAttribute("recipes", recipeService.getRecipes());
 		
         return "index";
--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -10,6 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Ingredient {
 
@@ -29,50 +32,11 @@ public class Ingredient {
 	public Ingredient() {
 	}
 
-	public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+	public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
 		this.description = description;
 		this.amount = amount;
 		this.uom = uom;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public BigDecimal getAmount() {
-		return amount;
-	}
-
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
-
-	public Recipe getRecipe() {
-		return recipe;
-	}
-
-	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
 	}
 
-	public UnitOfMeasure getUom() {
-		return uom;
-	}
-
-	public void setUom(UnitOfMeasure uom) {
-		this.uom = uom;
-	}
-		
 }
